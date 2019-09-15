3d25ab42e5cf9c907049567406b2b7debaabbc09
Mohammed Nasir <mnehaal@gmail.com>
2019-09-15 02:15:16 -0400

    server bugfixes

--- Server/http_server.py
+++ Server/http_server.py
@@ -11,29 +11,15 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     # GET (audio)
     def do_GET(self):
         # Send response status code
-        self.send_response(200)
+        self.send_response(404)
         # Send headers
-        self.send_header('Content-type', 'audio/wav')
         self.end_headers()
-        # # Send message back to client
-        # message = "Hello world!"
-        # # Write content as utf-8 data
-        # self.wfile.write(bytes(message, "utf8"))
-        with open('chime.wav', 'rb') as content:
-            shutil.copyfileobj(content, self.wfile)
         return
 
     # POST (image, audio)
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        self.send_response(200)
+        self.send_response(204)
         self.end_headers()
-        response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
-        self.wfile.write(response.getvalue())
         return
 
     def do_PUT(self):
@@ -41,14 +27,14 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         filename = os.path.basename(self.path)
         file_length = int(self.headers['Content-Length'])
 
-        if filename == "image.jpg":
+        if filename[-4:] == ".jpg":
             with open(filename, 'wb') as output_file:
                 output_file.write(self.rfile.read(file_length))
             self.send_response(200, 'Awaiting audio')
             self.end_headers()
             return
 
-        elif filename == "command.wav":
+        elif filename[-4:] == ".wav":
             with open(filename, 'wb') as output_file:
                 output_file.write(self.rfile.read(file_length))
 
